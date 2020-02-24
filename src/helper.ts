05457eee3b5fdd11151dd32ef70c0265c659cd43
min9nim <min9nim@gmail.com>
2020-02-25 03:20:56 +0900

    update warn message

--- src/helper.ts
+++ src/helper.ts
@@ -57,7 +57,8 @@ export class TimeManager {
   timeLabels = {}
   time(label: string) {
     if (this.timeLabels[label]) {
-      console.warn(`[error] duplicate label [${label}]`)
+      // console.warn(`[error] duplicate label [${label}]`)
+      consoleTransport('warn', '', `[warn] duplicate label '${label}'`)
       return
     }
     this.timeLabels[label] = Date.now()
@@ -65,7 +66,8 @@ export class TimeManager {
   timeEnd(label: string) {
     const asisTime = this.timeLabels[label]
     if (!asisTime) {
-      console.warn(`[error] Not found label [${label}]`)
+      // console.warn(`[error] Not found label [${label}]`)
+      consoleTransport('warn', '', `[warn] Not found label '${label}'`)
       return
     }
     this.timeLabels[label] = undefined
